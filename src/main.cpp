8908896981659d12856deecd4b62610ff8f9a8fd
Parnassius <Parnassius@users.noreply.github.com>
2024-03-02 16:58:38 +0100

    Write default screenshot and recording paths when running in Flatpak

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
 #include <QApplication>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QProcess>
 #include <QQmlApplicationEngine>
 
 
 int main(int argc, char *argv[]) {
+    if (QFileInfo::exists(QStringLiteral("/.flatpak-info"))) {
+        const QDir configDir(qEnvironmentVariable("XDG_CONFIG_HOME"));
+        if (!configDir.exists(QStringLiteral("options.json"))) {
+            QProcess userDirProcess;
+
+            userDirProcess.start(QStringLiteral("xdg-user-dir"), {QStringLiteral("VIDEOS")});
+            userDirProcess.waitForFinished();
+            const QString userDirVideo = QString::fromUtf8(userDirProcess.readAllStandardOutput()).trimmed();
+
+            userDirProcess.start(QStringLiteral("xdg-user-dir"), {QStringLiteral("PICTURES")});
+            userDirProcess.waitForFinished();
+            const QString userDirPictures = QString::fromUtf8(userDirProcess.readAllStandardOutput()).trimmed();
+
+            const QJsonObject optionsObj {
+                {QStringLiteral("recordingsPath"), userDirVideo + QStringLiteral("/SysDVR")},
+                {QStringLiteral("screenshotsPath"), userDirPictures + QStringLiteral("/SysDVR")}
+            };
+            const QJsonDocument optionsJson(optionsObj);
+            QFile optionsFile(configDir.filePath(QStringLiteral("options.json")));
+            optionsFile.open(QFile::WriteOnly);
+            optionsFile.write(optionsJson.toJson());
+        }
+    }
+
     QApplication app(argc, argv);
 
     QApplication::setOrganizationName(QStringLiteral("sysdvr-qt"));
